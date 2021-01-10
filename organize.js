52cdad68a7f8ef150b2b462fe95fe711a7f1fc5f
Cheyenne Nickelberry <30728389+Cheynick138@users.noreply.github.com>
2021-01-09 21:58:47 -0500

    Add files via upload

---
+++ organize.js
@@ -0,0 +1,22 @@
+//variables
+var inputHours = ["9am", "10am", "11fam", "12pm", "1pm", "2pm", "3pm"];
+
+
+//code will create the time slots.
+for (var i = 0; i < inputHours.length; i++) {
+    var timeSlot = $("<div>");
+    timeSlot.addClass("hour col-xs-2 col-sm-2 col-md-2 col-lg-2");
+    timeSlot.text(inputHours[i]);
+    var inputSched = $("<div>");
+    inputSched.addClass("future col-xs-9 col-sm-9 col-md-9 col-lg-9 description");
+    var saveButton = ("<button>");
+    saveButton.addClass("col-xs-1 col-sm-1 col-md-1 col-lg-1 fas fa-save btn-block saveBtn");
+    var tRow = $("<div>");
+    tRow.addClass("row time-block");
+    tRow.append(timeSlot, inputSched, saveButton);
+
+}
+
+
+
+  //inputHours[i].append(moment({hour}).format ("h a"));
